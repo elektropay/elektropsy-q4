ff9715fe057791070b73d29315bbc369b7aec6c9
“Harish” <hariharan.g@tranxitgroup.com>
2022-09-24 18:29:44 +0530

    css

--- next.config.js
+++ next.config.js
@@ -1,7 +1,20 @@
 /** @type {import('next').NextConfig} */
+const webpack = require("webpack");
 const nextConfig = {
   reactStrictMode: true,
+  distDir: 'build',
   swcMinify: true,
-}
+  trailingSlash: true,
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, nextRuntime, webpack }) => {
+    config.plugins.push(
+      new webpack.ProvidePlugin({
+        $: "jquery",
+        jQuery: "jquery",
+        "window.jQuery": "jquery",
+      })
+    );
+    return config;
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
